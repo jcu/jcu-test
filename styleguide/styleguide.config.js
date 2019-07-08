e7160f7ed52140b154f1ea860000439a15c38d15
Mahendra Patel <mkumar.a.patel@gmail.com>
2019-07-08 11:18:19 +1000

    Added title and Logo in StyleGuide
    Added title and Logo in StyleGuide

--- styleguide/styleguide.config.js
+++ styleguide/styleguide.config.js
@@ -1,4 +1,9 @@
+const path = require('path');
+const { theme, styles } = require('./components/styles')
 module.exports = {
+  title: "JCU React Component Library",
+  theme,
+  styles,
   sections: [
     {
       name: 'JCU React Component Library',
@@ -23,4 +28,7 @@ module.exports = {
     favicon: 'https://www.jcu.edu.au/__data/assets/file/0006/642327/favicon.ico?v=0.1.0'
   },
   webpackConfig: require('./webpack.styleguide.js'),
+  styleguideComponents: {
+    LogoRenderer: path.join(__dirname, 'components/Logo')
+  }
 };
